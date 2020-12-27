c426ab9862f16b10bbada1b0805eb2182a7c6ed1
hoangpm6814 <hoangpm6814@gmail.com>
2020-12-27 15:38:25 +0700

    create and implement ICountryRepo

---
+++ BookProject/Services/CountryRepository.cs
@@ -0,0 +1,37 @@
+﻿using BookProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookProject.Services
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private BookDbContext _bookDbContext;
+
+        public CountryRepository(BookDbContext bookDbContext)
+        {
+            _bookDbContext = bookDbContext;
+        }
+        public ICollection<Author> GetAuthorsFromACountry(int countryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Country> GetCountries()
+        {
+            return _bookDbContext.Countries.OrderBy(c => c.Name).ToList();
+        }
+
+        public Country GetCountry(int countryId)
+        {
+            return _bookDbContext.Countries.Where(c => c.Id == countryId).FirstOrDefault();
+        }
+
+        public Country GetCountryOfAnAuthor(int authorId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookProject/Services/ICountryRepository.cs
@@ -0,0 +1,17 @@
+﻿using BookProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookProject.Services
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int countryId);
+        Country GetCountryOfAnAuthor(int authorId);
+        ICollection<Author> GetAuthorsFromACountry(int countryId);
+        bool CountryExists(int countryId);
+    }
+}
